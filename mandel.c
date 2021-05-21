1801b90f52805a7d1d4dbd5d3674bea11468a599
thisisjaskaran <sodhi.jaskaransingh@gmail.com>
2021-05-22 02:32:48 +0530

    mandel debugged

--- mandel.c
+++ mandel.c
@@ -1,8 +1,8 @@
+#include <stdio.h>
 #include <omp.h>
 #define NPOINTS 1000
-#define MXITR 1000
+#define MXITR 10000
 
-void testpoint(void);
 struct d_complex
 {
     double r;
@@ -12,30 +12,14 @@ struct d_complex
 struct d_complex c;
 int numoutside = 0;
 
-int main()
-{
-    int i, j;
-    double area, error, eps = 1.0e-5;
-#pragma omp parallel for default(shared) private(c, eps)
-    for (i = 0; i < NPOINTS; i++)
-    {
-        for (j = 0; j < NPOINTS; j++)
-        {
-            c.r = -2.0 + 2.5 * (double)(i) / (double)(NPOINTS) + eps;
-            c.i = 1.125 * (double)(j) / (double)(NPOINTS) + eps;
-            testpoint();
-        }
-    }
-    area = 2.0 * 2.5 * 1.125 * (double)(NPOINTS * NPOINTS - numoutside) / (double)(NPOINTS * NPOINTS);
-    error = area / (double)NPOINTS;
-}
-
-void testpoint(void)
+void testpoint(struct d_complex c)
 {
     struct d_complex z;
     int iter;
     double temp;
 
+    // z.r = c.r;
+    // z.i = c.i;
     z = c;
     for (iter = 0; iter < MXITR; iter++)
     {
@@ -44,8 +28,28 @@ void testpoint(void)
         z.r = temp;
         if ((z.r * z.r + z.i * z.i) > 4.0)
         {
+#pragma omp atomic
             numoutside++;
             break;
         }
     }
+}
+
+int main()
+{
+    int i, j;
+    double area, error, eps = 1.0e-5;
+#pragma omp parallel for default(none) private(c, j) firstprivate(eps)
+    for (i = 0; i < NPOINTS; i++) //the loop index of i (since it is the ine pragma omp for is applied to) is implicitly made private
+    {
+        for (j = 0; j < NPOINTS; j++) //but the above policy does not apply to j
+        {
+            c.r = -2.0 + 2.5 * (double)(i) / (double)(NPOINTS) + eps;
+            c.i = 1.125 * (double)(j) / (double)(NPOINTS) + eps;
+            testpoint(c);
+        }
+    }
+    area = 2.0 * 2.5 * 1.125 * (double)(NPOINTS * NPOINTS - numoutside) / (double)(NPOINTS * NPOINTS);
+    error = area / (double)NPOINTS;
+    printf("Area = %lf, Error = %lf\n", area, error);
 }
